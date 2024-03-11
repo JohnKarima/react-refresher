933595cf28a58937eafc7206d13f01e18d02ed20
JohnKarima <karimajohn24@gmail.com>
2024-03-11 06:40:01 +0300

    Create and import card component

---
+++ src/Card.jsx
@@ -0,0 +1,14 @@
+function Card () {
+
+    return(
+        <div className="card">
+        <img src="https://via.placeholder.com/150" alt="Profile Picture"></img>
+        <h2>Zabini</h2>
+        <p>Sorcerer/Musician</p>
+
+        </div>
+    )
+
+}
+
+export default Card
