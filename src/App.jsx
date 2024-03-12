fd5a2a0045a58b60bdd18adffbc507e46905032c
JohnKarima <karimajohn24@gmail.com>
2024-03-12 20:54:36 +0300

    Create and style card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import Card from './Card'
 
-
-function App () {
-
-    return(
-        <Card></Card>
+function App() {
+    return (
+        <>
+            <Card></Card>
+            <Card></Card>
+            <Card></Card>
+        </>
     )
-
 }
 
 export default App
--- src/Card.jsx
+++ src/Card.jsx
@@ -2,9 +2,9 @@ function Card () {
 
     return(
         <div className="card">
-        <img src="https://via.placeholder.com/150" alt="Profile Picture"></img>
-        <h2>Zabini</h2>
-        <p>Sorcerer/Musician</p>
+        <img className="card-image" src="https://via.placeholder.com/150" alt="Profile Picture"></img>
+        <h2 className="card-title">Zabini</h2>
+        <p className="card-text">Sorcerer/Musician/Professional Paint Watcher</p>
 
         </div>
     )
