d0b4f9248525c7a58e8d2837b8be5345ce337c3c
JohnKarima <karimajohn24@gmail.com>
2024-03-14 06:29:39 +0300

    Create and render reuseable lists

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
-import UserGreeting from './UserGreeting'
+import List from './List'
 
 function App() {
+    const fruits = [
+        { id: 1, name: 'apple', calories: 95 },
+        { id: 2, name: 'orange', calories: 45 },
+        { id: 3, name: 'banana', calories: 105 },
+        { id: 4, name: 'coconut', calories: 159 },
+        { id: 5, name: 'pineapple', calories: 37 },
+    ]
+
+    const vegetables = [
+        { id: 6, name: 'potatoes', calories: 95 },
+        { id: 7, name: 'celery', calories: 45 },
+        { id: 8, name: 'carrots', calories: 105 },
+        { id: 9, name: 'corn', calories: 159 },
+        { id: 10, name: 'broccoli', calories: 37 },
+    ]
+
     return (
         <>
-           <UserGreeting isLoggedIn={true} ></UserGreeting>
+            <List items={fruits} category="fruits"></List>
+            <List items={vegetables} category="vegetables"></List>
         </>
     )
 }
