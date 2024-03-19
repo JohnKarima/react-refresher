e22b5a22aa0dda9b6f31dc4561591cbb21784c2d
JohnKarima <karimajohn24@gmail.com>
2024-03-19 06:19:47 +0300

    Woked with click events and how to access their properties for styling and other purposes

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,11 @@
-import List from './List'
+// import Button from './Button'
+import ProfilePicture from './ProfilePicture'
+
 
 function App() {
-    const fruits = [
-        { id: 1, name: 'apple', calories: 95 },
-        { id: 2, name: 'orange', calories: 45 },
-        { id: 3, name: 'banana', calories: 105 },
-        { id: 4, name: 'coconut', calories: 159 },
-        { id: 5, name: 'pineapple', calories: 37 },
-    ]
-
-    const vegetables = [
-        { id: 6, name: 'potatoes', calories: 95 },
-        { id: 7, name: 'celery', calories: 45 },
-        { id: 8, name: 'carrots', calories: 105 },
-        { id: 9, name: 'corn', calories: 159 },
-        { id: 10, name: 'broccoli', calories: 37 },
-    ]
-
     return (
         <>
-            <List items={fruits} category="fruits"></List>
-            <List items={vegetables} category="vegetables"></List>
+            <ProfilePicture></ProfilePicture>
         </>
     )
 }
