c2fe3dbf71c58ff619f0ec5a692f142f859e59e9
JohnKarima <karimajohn24@gmail.com>
2024-03-22 06:17:37 +0300

    Worked with the onChange event handler to make responsive forms

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-import MyComponent from './assets/MyComponent'
-import Counter from './Counter'
+import MyComponent from './MyComponent'
 
 function App() {
     return (
         <>
-            <Counter/>
+            <MyComponent/>
         </>
     )
 }
